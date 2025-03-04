5e749aa018a0109eb449f972515f01827ea4c54c
labs2 <labs2@cin.ufpe.br>
2025-03-04 20:54:54 -0300

    Adjustments in app config file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,8 +5,8 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideAnimationsAsync() , provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideAnimationsAsync() , provideHttpClient(withFetch())]
 };
